cf6d5deb712f8c90c035ff113912fde3c05d9c0c
SnowSword1010 <jmayank2001@gmail.com>
2020-06-15 00:26:00 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navbar from './components/navbar.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>sollicitudin nibh sit amet commodo</h1>
+        <Navbar></Navbar>
+      </header>
+    </div>
+  );
+}
+
+export default App;
